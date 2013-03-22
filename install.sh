7919e6e3094535e1921076e31cf6a882045f16a8
lbj96347 <cashlee96347@gmail.com>
2013-03-22 12:36:52 +0800

    [mod]Add install shell script into repo

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Welcome to CashLee Macbook Config enjoy this process!"
+
+#Cpoy vim config files 
+cp vimrc ~/.vimrc
+echo "Copyed vim config file"
+
+#Copy vim plugins 
+cp vim ~/.vim
+echo "Copyed vim config file"
+
+#Copy bash_profile
+cp bash_profile ~/.bash_profile
+echo "Copyed vim config file"
+
